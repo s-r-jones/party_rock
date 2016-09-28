d606bc71986705a3d9cd6c3d614d4963f0b16621
Sam Jones <bsamjones@gmail.com>
2016-09-27 22:56:30 -0500

    add nav buttons, tableView, and banner

---
+++ party_rock/party_rock/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  party_rock
+//
+//  Created by Sam Jones on 9/27/16.
+//  Copyright © 2016 Sam Jones. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+
+}
+
